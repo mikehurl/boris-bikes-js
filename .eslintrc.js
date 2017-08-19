4f4da226c1f5f94881b85470d25acba3660f3a3b
Mike Hurl <mike.hurl@capgemini.com>
2017-08-19 17:14:39 +0100

    add additonal options to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": { "es6": true },
+    "parserOptions": {
+      "ecmaFeatures": {
+        "impliedStrict": true
+      }
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "mocha": true,
+    }
 };
