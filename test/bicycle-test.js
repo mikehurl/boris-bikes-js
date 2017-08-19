48b3e1312a5fb53cd0619b6aee799a755067f337
Mike Hurl <mike.hurl@capgemini.com>
2017-08-19 19:05:12 +0100

    test passes, Bicycle has isWorking method

---
+++ test/bicycle-test.js
@@ -0,0 +1,11 @@
+import Bicycle from '../src/bicycle';
+
+const expect = require('chai').expect;
+
+describe('Bicycle', () => {
+  describe('isWorking', () => {
+    it('should respond to .isWorking', () => {
+      expect(new Bicycle()).to.respondTo('isWorking');
+    });
+  });
+});
